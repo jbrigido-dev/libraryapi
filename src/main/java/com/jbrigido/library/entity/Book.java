f1b53f19f767b432050db00e7c9ca8197980eab4
JonDEv <jbrigidoperez@outlook.com>
2026-07-06 22:00:18 -0600

    Book entity created

---
+++ src/main/java/com/jbrigido/library/entity/Book.java
@@ -0,0 +1,43 @@
+package com.jbrigido.library.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "book")
+@Getter
+@Setter
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 13, unique = true)
+    private String isbn;
+
+    @Column(nullable = false)
+    @Min(value = 1)
+    private Integer edition;
+
+    @Column(nullable = false, length = 50)
+    private String language;
+
+    @Column(nullable = false)
+    private LocalDate publishDate;
+
+    @Column(nullable = false, length = 100)
+    private String publisher;
+}

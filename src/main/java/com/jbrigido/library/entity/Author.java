7ab38988ad6f5754cd7c7d3874fc1a5986c26248
JonDEv <jbrigidoperez@outlook.com>
2026-07-06 21:05:52 -0600

    Author entity created

---
+++ src/main/java/com/jbrigido/library/entity/Author.java
@@ -0,0 +1,36 @@
+package com.jbrigido.library.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "author")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String lastName;
+
+    @Column(nullable = false)
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    private String nationality;
+
+}

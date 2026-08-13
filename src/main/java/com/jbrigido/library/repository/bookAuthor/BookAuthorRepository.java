64b2697b7954bf451e6cc57beef13ea8d3a16731
JonDEv <jbrigidoperez@outlook.com>
2026-08-13 17:08:42 -0600

    feat: implement JWT generation and validation service

--- src/main/java/com/jbrigido/library/repository/bookAuthor/BookAuthorRepository.java
+++ src/main/java/com/jbrigido/library/repository/bookAuthor/BookAuthorRepository.java
@@ -2,7 +2,6 @@ package com.jbrigido.library.repository.bookAuthor;
 
 import com.jbrigido.library.entity.Book;
 import com.jbrigido.library.entity.BookAuthor;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
---
+++ src/main/java/com/jbrigido/library/service/JwtService.java
@@ -0,0 +1,69 @@
+package com.jbrigido.library.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+    @Value("${jwt.time.expiration}")
+    private long expiration;
+
+    private String generateToken(UserDetails user) {
+
+        Date issued = new Date();
+        Date expirationDate = new Date(issued.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .issuedAt(new Date())
+                .expiration(expirationDate)
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(
+                Decoders.BASE64.decode(secretKey)
+        );
+    }
+
+    private Claims extractAllClaims(String token) {
+        SecretKey key = getSignInKey();
+        return Jwts.parser().
+                decryptWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private String extractUsername(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getSubject();
+    }
+
+    private Date extractExpiration(String token) {
+        Claims claims = extractAllClaims(token);
+        return claims.getExpiration();
+    }
+
+    public boolean isValidToken(String token, UserDetails user) {
+            String username = extractUsername(token);
+            return user.getUsername().equals(username) && !isExpiredToken(token);
+    }
+
+    private boolean isExpiredToken(String token) {
+        Date expiration = extractExpiration(token);
+        return expiration.before(new Date());
+    }
+}
